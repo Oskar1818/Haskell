de19cf4a4057e16426e46cf0f6c836360cd457f5
Valter Miari <valtermiari@MacBook-Pro.local>
2020-09-11 17:14:43 +0200

    Newest BJ file

--- Blackjack/Blackjack.hs
+++ Blackjack/Blackjack.hs
@@ -1,42 +1,41 @@
--- Lab: Blackjack - part A
--- Authors: Clara Josefsson, Oskar Sturebrand, Valter Miari
--- Lab group: 59
-
 module Blackjack where
 
 import Cards
 import RunGame
 import Test.QuickCheck hiding (shuffle)
 
+-- Size hand2 evaluated by hand
+{-  size hand2
+    = size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
+    = 1 + size ((Card Jack Spades : []))
+    = 1 + 1 + size []
+    = 1 + 1 + 0
+    = 2   -}
 
--- Task A1
 hand2 :: Hand
 hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
 
+-- Describes the steps size takes
 sizeSteps :: [Int]
 sizeSteps = [ size hand2
             , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
             , 1 + size ((Card Jack Spades : []))
             , 1 + 1 + size []
             , 1 + 1 + 0 -- might not be nececcary
-            , 2 ]
+            , 2
+            ]
 
-
--- Definitions
--- Favorite card
+-- A test card
 aCard1 :: Card
-aCard1 = Card Ace Spades
+aCard1 = (Card Ace Spades)
 
--- Another card
+-- A test card
 aCard2 :: Card
-aCard2 = Card (Numeric 3) Diamonds
+aCard2 = (Card (Numeric 9) Diamonds)
 
--- a Hand with two Cards, aCard1 and aCard2
+-- A test hand
 aHand :: Hand
-aHand = [Card (Numeric 7) Clubs, Card Queen Spades]
-
-
--- Task A2
+aHand = [aCard1, aCard2]
 
 -- to run in windows do
 -- chcp 65001
@@ -44,10 +43,12 @@ aHand = [Card (Numeric 7) Clubs, Card Queen Spades]
 displayCard :: Card -> String
 displayCard (Card r s) = displayRank r ++ " of " ++ displaySuit s
 
+-- shows the rank
 displayRank :: Rank -> String
 displayRank (Numeric n) = show n -- gives the rank of numeric cards
-displayRank r = show r -- gives the rank of suited cards
+displayRank r = show r -- gives the rank of suited card
 
+-- Displays the suits with unicode characters
 displaySuit :: Suit -> String
 displaySuit Hearts = "\9829 "
 displaySuit Spades = "\9824 "
@@ -55,9 +56,50 @@ displaySuit Diamonds = "\9830 "
 displaySuit Clubs = "\9827 "
 -- chcp 65001
 
+-- Takes a hand as input, declares a basecase for it to be able recursively go over
+-- the hand, then uses the help-functions to dislplay the cards on seperate lines
+-- with \n
 display :: Hand -> String
-display hand = unlines [displayCard card | card <- hand]
+display [] = ""
+display (x:xs) = displayCard x ++ "\n" ++ display xs
 
--- Task A3
+-- Declares the values of the ranks, as for the Ace; it evaluates to 1 in the
+-- numberOfAces function, so it is not nececcary here
+valueRank :: Rank -> Int
+valueRank (Numeric n) = n
+valueRank Ace         = 11
+valueRank _           = 10
+
+-- This function ses first if the list is empty and then returns a empty String
+-- then determines if there is an ace or not.
+numberOfAces :: Hand -> Int
+numberOfAces [] = 0
+numberOfAces (Card Ace _:restOfHand)  = 1 + numberOfAces restOfHand
+numberOfAces (_:restOfHand) = numberOfAces restOfHand
+
+-- sum [ 1 | card <- hand, (rank card) == Ace]
+
+-- value
+valueCard :: Card -> Int
+valueCard (Card r s) = valueRank r
+
+valueTot :: Hand -> Int
+valueTot [] = 0
+valueTot (x:xs) = valueCard x + valueTot xs
+
+-- This function calculates if the value overexceedes 21 and if so, if it contains an or several
+-- aces it converts them to 1 instead of 11.
 value :: Hand -> Int
-value = undefined
+value hand | valueTot hand > 21 = valueTot hand - (10 * numberOfAces hand)
+           | otherwise = valueTot hand
+
+
+gameOver :: Hand -> Bool
+gameOver hand = value hand > 21
+
+winner :: Hand -> Hand -> Player
+winner handGuest handBank | gameOver (handBank) = Guest
+                          | gameOver (handGuest) = Bank
+                          | value(handBank) > value(handGuest) = Bank
+                          | value(handGuest) > value(handBank) = Guest
+                          | value(handGuest) == value(handBank) = Bank
