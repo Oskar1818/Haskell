395e91593b659b6b5d988fa8266842570b039e5c
Oskar Sturebrand <oskar@Oskars-Air.lan>
2020-09-11 13:33:55 +0200

    ny mapp

--- Blackjack/Blackjack.hs
+++ Blackjack/Blackjack.hs
@@ -4,13 +4,7 @@ import Cards
 import RunGame
 import Test.QuickCheck hiding (shuffle)
 
-{-  size hand2
-    = size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
-    = 1 + size ((Card Jack Spades : []))
-    = 1 + 1 + size []
-    = 1 + 1 + 0
-    = 2   -}
-
+-- A1
 hand2 :: Hand
 hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
 
