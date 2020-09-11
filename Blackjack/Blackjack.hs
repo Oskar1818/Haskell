6cbf4175e82e323f71fc16b5494616ac137a199f
Oskar Sturebrand <oskar@Oskars-Air.lan>
2020-09-11 13:56:12 +0200

    Begining of A2

--- Blackjack/Blackjack.hs
+++ Blackjack/Blackjack.hs
@@ -1,10 +1,15 @@
+-- Lab: Blackjack - part A
+-- Authors: Clara Josefsson, Oskar Sturebrand, Valter Miari
+-- Lab group: 59
+
 module Blackjack where
 
 import Cards
 import RunGame
 import Test.QuickCheck hiding (shuffle)
 
--- A1
+
+-- Task A1
 hand2 :: Hand
 hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
 
@@ -14,5 +19,26 @@ sizeSteps = [ size hand2
             , 1 + size ((Card Jack Spades : []))
             , 1 + 1 + size []
             , 1 + 1 + 0 -- might not be nececcary
-            , 2
-            ]
+            , 2 ]
+
+
+-- Definitions
+-- Favorite card
+aCard1 :: Card
+aCard1 = Card Ace Spades
+
+-- Another card
+aCard2 :: Card
+aCard2 = Card (Numeric 3) Diamonds
+
+-- a Hand with two Cards, aCard1 and aCard2
+aHand :: Hand
+aHand = [Card (Numeric 7) Clubs, Card Queen Spades]
+
+
+-- Task A2
+display :: Hand -> String
+-- \9829 == Hearts
+-- \9824 == Spades
+-- \9830 == Diamonds
+-- \9827 == Clubs
