2b954a1ed2a7a486e1644e6fd1849f7e3c0f3821
Valter Miari <valtermiari@macbook-pro.lan>
2020-09-30 10:55:59 +0200

    new file

--- Tetris/src/Shapes.hs
+++ Tetris/src/Shapes.hs
@@ -1,6 +1,3 @@
---Lab Tetris Part A
---Group 59
---Authors: Oskar Sturebrand, Valter Miari, Clara Josefsson
 
 -- | Types and functions for shapes. The list of all tetris pieces.
 
@@ -11,13 +8,14 @@ import Data.Maybe (isNothing)
 import Test.QuickCheck
 
 -- * Shapes
+
 type Square = Maybe Colour
 
 data Colour = Black | Red | Green | Yellow | Blue | Purple | Cyan | Grey
   deriving (Eq, Bounded, Enum, Show)
 
 -- | A geometric shape is represented as a list of lists of squares. Each square
--- | can be empty or filled with a block of a specific colour.
+-- can be empty or filled with a block of a specific colour.
 
 data Shape = S [Row] deriving (Eq)
 type Row   = [Square]
@@ -45,7 +43,7 @@ instance Show Shape where
 -- * The shapes used in the Tetris game
 
 -- | All 7 tetrominoes (all combinations of 4 connected blocks),
--- | see <https://en.wikipedia.org/wiki/Tetromino>
+-- see <https://en.wikipedia.org/wiki/Tetromino>
 allShapes :: [Shape]
 allShapes = [S (makeSquares s) | s <- shapes]
    where
@@ -76,13 +74,9 @@ allShapes = [S (makeSquares s) | s <- shapes]
                "SS",
                " S"]]
 
-
-
--- * Some advanced functions ;)
+-- * Some simple functions
 
 -- ** A1
--- | emptyShape uses the standard function replicate who generates r elements of
--- | emptyRow s.
 emptyShape :: (Int, Int) -> Shape
 emptyShape (r, s) =  S (replicate r (emptyRow s))
 
@@ -91,126 +85,174 @@ emptyRow r = replicate r Nothing
 
 
 -- ** A2
+
 -- | The size (width and height) of a shape
 shapeSize :: Shape -> (Int, Int)
 shapeSize (S (x:xs)) = (length(x), length(x:xs))
 
-
 -- ** A3
--- | Determines the length of a list comprehension, where in the list comp
--- | it iterates over the number of elements that isn't Nothing.
+
+-- | Count how many non-empty squares a shape contains
 blockCount :: Shape -> Int
 blockCount (S (x:xs)) = length [x | x <- join(S (x:xs)), x /= Nothing]
+
+aShape :: Shape
+aShape = emptyShape(3, 2)
+
+aShape2 :: Shape
+aShape2 = emptyShape(1, 0)
 
 join :: Shape -> [Square]
 join (S (x:xs)) = concat (x:xs)
 
-
 -- * The Shape invariant
 
---  A4
+-- ** A4
 -- | Shape invariant (shapes have at least one row, at least one column,
--- | and are rectangular)
+-- and are rectangular)
 prop_Shape :: Shape -> Bool
 prop_Shape (S (x:xs)) = shapeSize (S (x:xs)) >= (1, 1)
-otherwise = False
 
 
 -- * Test data generators
 
 -- ** A5
 -- | A random generator for colours
-genColour :: Gen Colour -- sample genColour
+genColour :: Gen Colour
 genColour = elements [Black, Red, Green, Yellow, Blue, Purple, Cyan, Grey]
 
 instance Arbitrary Colour where
   arbitrary = genColour
 
-
 -- ** A6
 -- | A random generator for shapes
-genShape :: Gen Shape -- | sample genShape
+genShape :: Gen Shape
 genShape = elements allShapes
 
 instance Arbitrary Shape where
   arbitrary = genShape
-
 
 -- * Transforming shapes
 
 -- ** A7
 -- | Rotate a shape 90 degrees
 rotateShape :: Shape -> Shape
--- | rotateShape = S . transpose . reverse
--- | rotateShape ransposes the rows and columns of reverse of the list xs.
-rotateShape (S xs) = (S (transpose(reverse xs)))
--- | rotateShape S xs = S . transpose . reverse xs
+rotateShape (S (xs)) = (S (transpose(reverse xs)))
+
 
 -- ** A8
 -- | shiftShape adds empty squares above and to the left of the shape
--- | i steps down and j steps to the right
+-- i är antal steg nedåt.
+-- j är antal steg åt höger
 shiftShape :: (Int, Int) -> Shape -> Shape
-shiftShape (i, j)  shape = S (shiftDown (i, j) (S (shiftRight (i, j) shape)) ++ shiftRight (i, j) shape)
+shiftShape (i, j)  (S (xs)) = S (shiftDown (i, j) (S (shiftRight (i, j) (S (xs)))) ++ shiftRight (i, j) (S (xs)))
 
 shiftRight :: (w, Int) -> Shape -> [Row]
-shiftRight (_, j) (S xs) = [(emptyRow j ++ x) | x <- xs]
+shiftRight (_, j) (S (xs)) = [(emptyRow j ++ x) | x <- xs]
 
 shiftDown :: (Int, w) -> Shape -> [Row]
-shiftDown (i, _) (S xs) = emp
-  where (S emp) = emptyShape (i, shapeWidth (S xs))
+shiftDown (i,_) (S (xs)) = emp
+  where (S emp) = emptyShape (i, (shapeWidth (S (xs))))
 
--- | returns width of a shape
 shapeWidth :: Shape -> Int
-shapeWidth = length . head . rows
--- | other ways to write the function, worth remebering
-  -- | shapeWidth s = length (head (rows s))
-  -- | shapeWidth (S (x:xs)) = length(x)
-
+shapeWidth (S (x:xs)) = length(x)
 
 -- ** A9
 -- | padShape adds empty sqaure below and to the right of the shape
--- | shiftShape function is used on a 180° rotated shape,
--- | which is then rotated 180° again to generate the desired shape
 padShape :: (Int, Int) -> Shape -> Shape
 padShape (i, j) s = doubleRotate (shiftShape (i, j) (doubleRotate s))
 
 doubleRotate :: Shape -> Shape
 doubleRotate s = rotateShape (rotateShape s)
 
-
 -- ** A10
--- | pad a shape to a given size by
--- | adding differance between desired size and shape size, using shiftShape.
+-- | pad a shape to a given size
 padShapeTo :: (Int, Int) -> Shape -> Shape
-padShapeTo (i, j) shape = shiftShape (i - c, j - r) shape
+padShapeTo (i, j) shape = padShape (i - r, j - c) shape
   where (c, r) = shapeSize shape
-
 
 -- * Comparing and combining shapes
 
 -- ** B1
+
 -- | Test if two shapes overlap
---overlaps :: Shape -> Shape -> Bool
---overlaps (S xs) (S ys) = or [rowsOverlap x y| x <- xs, y <- ys]
+overlaps :: Shape -> Shape -> Bool
+overlaps (S sh1) (S sh2) = or $ zipWith rowsOverlap sh1 sh2
 
 rowsOverlap :: Row -> Row -> Bool
 rowsOverlap xs ys = or [(x /= Nothing) && (y /= Nothing) | x <- xs, y <- ys]
 
-overlaps :: Shape -> Shape -> Bool
-overlaps (S sh1) (S sh2) = or $ zipWith rowsOverlap sh1 sh2
-
-
-
-
-
-
+-- ** B2
 -- ** B2
 -- | zipShapeWith, like 'zipWith' for lists
+
+-- (.) :: (b -> c) -> (a -> b) -> (a -> c)
+-- :: ((a -> b -> c) -> [a] -> [b] -> [c]) -> ((a -> b -> c) -> [a] -> [b] -> [c]) -> ?
+
+
+{- f :: a -> b
+
+
+f == (\x -> f x) -}
+
 zipShapeWith :: (Square->Square->Square) -> Shape -> Shape -> Shape
-zipShapeWith = error "A12 zipShapeWith undefined"
+zipShapeWith f (S s1) (S s2) = S $ zipWith (zipWith f) s1 s2 --zipShape . zipShape'
+
+  {- where
+    zipRows :: (Square -> Square -> Square) -> Row -> Row -> Row
+    zipRows f row1 row2 = zipWith f row1 row2 -}
+
+{-
+zipShape :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipShape f xs ys = S . map ((x,y) ->(zipShape' f) x y) . zip xs $ ys
+
+zipShape' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipShape' f xs ys = map ((x,y) -> f x y) . zip xs $ ys
+-}
+{-
+zipShapeWith :: (Square->Square->Square) -> Shape -> Shape -> Shape
+zipShapeWith = zipShape . zipShape
+
+zipShape :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipShape f xs ys = map ((x,y) -> f x y) . zip xs $ ys
+-}
+--map (S . xs) [list]
+
+
+blackClashes :: Shape -> Shape -> Shape
+blackClashes s1 s2 = zipShapeWith clash s1 s2
+ where
+  clash :: Square -> Square -> Square
+  clash Nothing Nothing = Nothing
+  clash Nothing s       = s
+  clash s       Nothing = s
+  clash (Just c1) (Just c2) = Just Black
+
 
 -- ** B3
 -- | Combine two shapes. The two shapes should not overlap.
--- | The resulting shape will be big enough to fit both shapes.
+-- The resulting shape will be big enough to fit both shapes.
 combine :: Shape -> Shape -> Shape
-s1 `combine` s2 = error "A13 zipShapeWith undefined"
+combine s1 s2
+  | s1 `overlaps` s2 = error "The shapes overlap"
+  | otherwise = (zipShapeWith f' (padShapeTo x s1) (padShapeTo x s2))
+
+ where
+   x = convert s1 s2
+
+
+
+
+f' :: Square -> Square -> Square
+f' Nothing Nothing = Nothing
+f' Nothing s       = s
+f' s       Nothing = s
+
+
+convert :: Shape -> Shape -> (Int, Int)
+convert (S s1) (S s2) = (max y b, max x a)
+ where
+  (x, y) = shapeSize (S s1)
+  (a, b) = shapeSize (S s2)
+
+-- | Types and functions for shapes. The list of all tetris pieces.
