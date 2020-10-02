fd8006e4974095be0e6eb8c8141af18f7e0f1f63
Oskar1818 <oskarsturebrand@icloud.com>
2020-10-02 15:00:09 +0200

    Shapes

--- Tetris/src/Shapes.hs
+++ Tetris/src/Shapes.hs
@@ -2,8 +2,8 @@
 -- Authors: Clara Josefsson, Oskar Sturebrand, Valter Miari
 -- Lab group: 59
 
--- | Types and functions for shapes. The list of all tetris pieces.
 
+-- | Types and functions for shapes. The list of all tetris pieces.
 module Shapes where
 
 import Data.List (transpose)
@@ -11,7 +11,6 @@ import Data.Maybe (isNothing)
 import Test.QuickCheck
 
 -- * Shapes
-
 type Square = Maybe Colour
 
 data Colour = Black | Red | Green | Yellow | Blue | Purple | Cyan | Grey
@@ -19,7 +18,6 @@ data Colour = Black | Red | Green | Yellow | Blue | Purple | Cyan | Grey
 
 -- | A geometric shape is represented as a list of lists of squares. Each square
 -- can be empty or filled with a block of a specific colour.
-
 data Shape = S [Row] deriving (Eq)
 type Row   = [Square]
 
@@ -27,7 +25,6 @@ rows :: Shape -> [Row]
 rows (S rs) = rs
 
 -- * Showing shapes
-
 showShape :: Shape -> String
 showShape s = unlines [showRow r | r <- rows s]
   where
@@ -44,7 +41,6 @@ instance Show Shape where
   showList ss r = unlines (map show ss) ++ r
 
 -- * The shapes used in the Tetris game
-
 -- | All 7 tetrominoes (all combinations of 4 connected blocks),
 -- see <https://en.wikipedia.org/wiki/Tetromino>
 allShapes :: [Shape]
@@ -77,6 +73,7 @@ allShapes = [S (makeSquares s) | s <- shapes]
                "SS",
                " S"]]
 
+
 -- * Some simple functions
 
 -- ** A1
@@ -88,44 +85,46 @@ emptyRow x' = replicate x' Nothing
 
 
 -- ** A2
-
 -- | The size (width and height) of a shape
 shapeSize :: Shape -> (Int, Int)
 shapeSize (S (x:xs)) = (length(x), length(x:xs))
 
 -- ** A3
-
 -- | Count how many non-empty squares a shape contains
 blockCount :: Shape -> Int
 blockCount (S (x:xs)) = length [x | x <- join(S (x:xs)), x /= Nothing]
 
-aShape :: Shape
-aShape = emptyShape(3, 2)
-
-aShape2 :: Shape
-aShape2 = emptyShape(1, 0)
-
+-- | Helperfunction to aid blockCount, returns individual list of squares from shape
 join :: Shape -> [Square]
 join (S (x:xs)) = concat (x:xs)
+
+
 
 -- * The Shape invariant
 
 -- ** A4
 -- | Shape invariant (shapes have at least one row, at least one column,
 -- and are rectangular)
+-- | divides the sum of all row lenghts with lenght of a single row, and
+-- then check if the average row is the same as the single row.
 prop_Shape :: Shape -> Bool
-prop_Shape (S (x:xs)) = shapeSize (S (x:xs)) >= (1, 1)
-
+prop_Shape (S xs) = rowL == avgL && columns
+ where
+   columns = shapeSize (S xs) >= (1, 1)
+   rowL = map length xs
+   avgL = div (sum rowL) (length rowL)
 
 -- * Test data generators
 
 -- ** A5
 -- | A random generator for colours
+-- | Uses the QuickCheck-function elements wich works as follows: [a] -> Gen a
 genColour :: Gen Colour
 genColour = elements [Black, Red, Green, Yellow, Blue, Purple, Cyan, Grey]
 
 instance Arbitrary Colour where
   arbitrary = genColour
+
 
 -- ** A6
 -- | A random generator for shapes
@@ -135,30 +134,35 @@ genShape = elements allShapes
 instance Arbitrary Shape where
   arbitrary = genShape
 
+
 -- * Transforming shapes
 
 -- ** A7
--- | Rotate a shape 90 degrees
+-- | Rotates a shape 90° clockwise
 rotateShape :: Shape -> Shape
 rotateShape (S xs) = (S (transpose(reverse xs)))
 
 
 -- ** A8
 -- | shiftShape adds empty squares above and to the left of the shape
--- x' är antal steg åt höger
--- y är antal steg åt nedåt
+-- x' = number of steps right
+-- y = number of steps down
 shiftShape :: (Int, Int) -> Shape -> Shape
 shiftShape (x', y)  s1 = S (shiftDown (x', y) (S (shiftRight (x', y) s1)) ++ shiftRight (x', y) s1)
 
-shiftRight :: (Int, w) -> Shape -> [Row]
-shiftRight (x', _) (S xs) = [(emptyRow x' ++ x) | x <- xs]
+-- Adds empty squares to the left of a shape
+shiftRight :: Int -> Shape -> [Row]
+shiftRight x' (S xs) = [(emptyRow x' ++ x) | x <- xs]
 
-shiftDown :: (w, Int) -> Shape -> [Row]
-shiftDown (_, y) (S xs) = emp
+-- Adds empty squares above a shape
+shiftDown :: Int -> Shape -> [Row]
+shiftDown y (S xs) = emp
   where (S emp) = emptyShape (shapeWidth (S xs), y)
 
+-- Returns the width of a given shape
 shapeWidth :: Shape -> Int
 shapeWidth (S (x:xs)) = length(x)
+
 
 -- ** A9
 -- | padShape adds empty sqaure below and to the right of the shape
@@ -167,80 +171,34 @@ shapeWidth (S (x:xs)) = length(x)
 padShape :: (Int, Int) -> Shape -> Shape
 padShape (x', y) s1 = doubleRotate (shiftShape (x', y) (doubleRotate s1))
 
+-- rotates a shape 180° clockwise
 doubleRotate :: Shape -> Shape
 doubleRotate s1 = rotateShape (rotateShape s1)
 
+
 -- ** A10
--- | pad a shape to a given size
+-- | pads a shape to a given size by adding empty rows and columns above and to the right.
 padShapeTo :: (Int, Int) -> Shape -> Shape
 padShapeTo (x', y) s1 = padShape (x' - c, y - r) s1
   where (c, r) = shapeSize s1
 
+
 -- * Comparing and combining shapes
 
 -- ** B1
-
--- | Test if two shapes overlap
+-- | Tests if two shapes overlaps
 overlaps :: Shape -> Shape -> Bool
 overlaps (S sh1) (S sh2) = or $ zipWith rowsOverlap sh1 sh2
 
+-- tests if two rows overlap
 rowsOverlap :: Row -> Row -> Bool
 rowsOverlap xs ys = or [(x /= Nothing) && (y /= Nothing) | x <- xs, y <- ys]
 
--- ** B2
--- ** B2
--- | zipShapeWith, like 'zipWith' for lists
-
--- (.) :: (b -> c) -> (a -> b) -> (a -> c)
--- :: ((a -> b -> c) -> [a] -> [b] -> [c]) -> ((a -> b -> c) -> [a] -> [b] -> [c]) -> ?
-
---zipShape :: (a -> b -> c) -> [a] -> [b] -> [c]
---zipShape f xs ys = S . map (\(x,y) -> f x y) . zip xs $ ys
-
-{- f :: a -> b
-
-zipShape :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipShape f xs ys = map (\(x,y) -> f x y) . zip xs $ ys
-
---map (S . xs) [list]
-
-{-
-blackClashes :: Shape -> Shape -> Shape
-blackClashes s1 s2 = zipShapeWith clash s1 s2
- where
-  clash :: Square -> Square -> Square
-  clash Nothing Nothing = Nothing
-  clash Nothing s       = s
-  clash s       Nothing = s
-  clash (Just c1) (Just c2) = Just Black
--}
-
-f == (\x -> f x) -}
 
 zipShapeWith :: (Square->Square->Square) -> Shape -> Shape -> Shape
 zipShapeWith f (S s1) (S s2) = S $ zipWith (zipWith f) s1 s2
 
-{- where
-  zipRows :: (Square -> Square -> Square) -> Row -> Row -> Row
-  zipRows f row1 row2 = zipWith f row1 row2 -}
-
-{-
-zipShape :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipShape f xs ys = S . map ((x,y) ->(zipShape' f) x y) . zip xs $ ys
-
-zipShape' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipShape' f xs ys = map ((x,y) -> f x y) . zip xs $ ys
--}
-{-
-zipShapeWith :: (Square->Square->Square) -> Shape -> Shape -> Shape
-zipShapeWith = zipShape . zipShape
-
-zipShape :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipShape f xs ys = map ((x,y) -> f x y) . zip xs $ ys
--}
---map (S . xs) [list]
-
-
+-- example function
 blackClashes :: Shape -> Shape -> Shape
 blackClashes s1 s2 = zipShapeWith clash s1 s2
  where
@@ -252,7 +210,7 @@ blackClashes s1 s2 = zipShapeWith clash s1 s2
 
 
 -- ** B3
--- | Combine two shapes. The two shapes should not overlap.
+-- Combine two shapes. The two shapes should not overlap.
 -- The resulting shape will be big enough to fit both shapes.
 combine :: Shape -> Shape -> Shape
 combine s1 s2
@@ -262,16 +220,12 @@ combine s1 s2
  where
    x = convert s1 s2
 
-
-
-
 f' :: Square -> Square -> Square
 f' Nothing Nothing = Nothing
 f' Nothing s       = s
 f' s       Nothing = s
--- f' s1      s2      = Nothing
 
-
+-- Returns the largest cordinate value, of each shape
 convert :: Shape -> Shape -> (Int, Int)
 convert (S s1) (S s2) = (max x a, max y b)
  where
