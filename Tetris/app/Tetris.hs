eed0ca3a4bbadfad68c7b14348e3650bf9c3ef67
Oskar Sturebrand <70581204+Oskar1818@users.noreply.github.com>
2020-10-09 15:52:10 +0200

    comments

--- Tetris/app/Tetris.hs
+++ Tetris/app/Tetris.hs
@@ -80,6 +80,7 @@ addWalls (S xs) = S $ addBlackRow (S xs) ++ map (addBlack) xs ++ addBlackRow (S 
 drawTetris :: Tetris -> Shape
 drawTetris (Tetris (v, s) w _) = addWalls (combine (shiftShape v s) w)
 
+-- Moves a shape by vector steps
 move :: Vector -> Tetris -> Tetris
 move v1 (Tetris (v2, s) w d) = (Tetris ((v1 `vAdd` v2), s) w d)
 
@@ -88,23 +89,29 @@ startTetris :: [Double] -> Tetris
 startTetris ds = Tetris (startPosition, shape1) (emptyShape wellSize) supply
   where
     shape1:supply = genShapes ds
+    -- picking a shape from the infinite list.
+
     ls = fromIntegral $ length allShapes
+    -- converts the lenght from int to double.
 
     randInt :: Double -> Int
     randInt d = floor $ d * ls
+    -- generates a relevant random double which is converted to int by floor.
 
     genShapes :: [Double] -> [Shape]
     genShapes (d:ds) = allShapes !! randInt d : genShapes ds
+    -- a recursive function wich creates an infinite list, which is ok
+    -- due to Haskell being lazy.
 
--- if :: Bool -> a -> a -> a
+
 -- returns the new state of the game, where the new tetris-state is modified
 -- by the move-function.
 tick :: Tetris -> Maybe (Int, Tetris)
 tick t@(Tetris (v, s) w sup) =
-                             if collision t'
-                              then dropNewPiece t -- call dropNewPiece
-                              else Just (0, t')     -- if that fails, game over.else Just (0, move (0, 1) t )
-  where t' = move (0, 1) t
+  if collision t'
+    then dropNewPiece t -- call dropNewPiece
+    else Just (0, t')     -- if that fails, game over.else Just (0, move (0, 1) t )
+      where t' = move (0, 1) t
 
 -- | React to input. The function returns 'Nothing' when it's game over,
 -- and @'Just' (n,t)@, when the game continues in a new state @t@.
@@ -117,19 +124,22 @@ stepTetris Rotate t = Just (0, rotatePiece t)
 
 
 
+-- Checks if there is any collisions in a tetris
 collision :: Tetris -> Bool
 collision (Tetris ((v1, v2), s) w _)
-  | place ((v1, v2), s) `overlaps` w     = True
-  | v1 + fst (shapeSize s) > wellWidth   = True
-  | v2 + snd (shapeSize s) > wellHeight  = True
-  | v1 < 0                               = True
-  | otherwise                            = False
+  | place ((v1, v2), s) `overlaps` w     = True -- new piece collide?
+  | v1 + fst (shapeSize s) > wellWidth   = True -- too far right?
+  | v2 + snd (shapeSize s) > wellHeight  = True -- too far down?
+  | v1 < 0                               = True -- too far left?
+  | otherwise                            = False -- no collision
 
+-- Moves a piece in the x dimension
 movePiece :: Int -> Tetris -> Tetris
 movePiece i t@(Tetris ((x, y), s) w sup)
   | collision $ move (i, 0) t = t
   | otherwise                 = move (i, 0) t
 
+-- rotates a shape in a tetris
 rotate:: Tetris -> Tetris
 rotate (Tetris ((v1, v2), s) w sup) = (Tetris ((v1, v2), (rotateShape s)) w sup)
 
@@ -139,6 +149,8 @@ rotatePiece t@(Tetris ((v1, v2), s) w sup)
   | otherwise            = rotate t
 
 
+-- Game ends if the new piece collides, else it returns a new random piece
+-- which is combined with the well, which has passed through clearLines.
 dropNewPiece :: Tetris -> Maybe (Int, Tetris)
 dropNewPiece t@(Tetris ((x, y), s) w sup)
   | collision tNew = Nothing -- game over
@@ -153,9 +165,13 @@ dropNewPiece t@(Tetris ((x, y), s) w sup)
 clearLines :: Shape -> (Int, Shape)
 clearLines (S xs) = (cleared, shape')
   where
+    -- adds empty rows ontop of the remaining well (shape)
     shape'    = shiftShape (0,cleared) (S remaining)  -- the new shape with the rows cleared and filled with empty rows
+    -- the that should be cleared, and added as empty rows
     cleared   = length xs - length remaining -- the amount of rows left
+    -- filters away the rows that are full and returns the remaining well
     remaining = filter notFull xs -- all rows that istn't null
 
+-- evaluates to false if a row is full; to make efficient use of filter func.
 notFull :: Row -> Bool
 notFull xs = not $ and $ map (\l -> l /= Nothing) xs
